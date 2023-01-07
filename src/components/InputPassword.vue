02952e15ab3e3bbb7f785200e1cfd2c419c7170c
blake S <design.harls@icloud.com>
2023-01-07 18:24:06 +0100

    added exercises vue basic

---
+++ src/components/InputPassword.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue';
+
+function PasswordShow() {
+     let password= document.getElementById("password");
+     if (password.type == "password") {
+        password.type = "text";
+        
+     }else{
+        password.type ="password";
+     }
+    
+}
+
+ 
+
+
+
+</script>
+
+<template>
+    <div class="container text-center input-password">
+        <h1 class="my-3">Exercise Input Password</h1>
+        <input type="password" name="password" id="password" class="form-control">
+        <div class="d-block">
+            <span id="show-password" @click="PasswordShow" class="mt-2 btn btn-warning">Show Password</span>
+        </div>
+    </div>
+</template>
+
+<style lang="css" scoped>
+    #show-password:hover {
+        cursor: pointer;
+        user-select: none;
+    }
+</style>
---
+++ src/views/Password.vue
@@ -0,0 +1,14 @@
+<script setup>
+import InputPassword from '../components/InputPassword.vue';
+
+
+
+
+
+</script>
+
+<template>
+  <main>
+    <InputPassword />
+  </main>
+</template>
